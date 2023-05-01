9346c169e7f8fb3bdbe887d402d80bc97fc1904b
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-05-01 19:29:40 +0530

    Reply entitly created

---
+++ src/main/java/com/app/blog/entity/Reply.java
@@ -0,0 +1,37 @@
+package com.app.blog.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Reply")
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long replyId;
+    private String replyMessage;
+    private Long likesCount;
+
+    public Long getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(Long replyId) {
+        this.replyId = replyId;
+    }
+
+    public String getReplyMessage() {
+        return replyMessage;
+    }
+
+    public void setReplyMessage(String replyMessage) {
+        this.replyMessage = replyMessage;
+    }
+
+    public Long getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(Long likesCount) {
+        this.likesCount = likesCount;
+    }
+}
